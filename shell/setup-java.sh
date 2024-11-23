206651ac31043b73b90cc7f285bd8b79d1ecb797
minmin <kobanminmin58@gmail.com>
2024-11-23 19:31:33 +0900

    feat: add setup java sh

---
+++ shell/setup-java.sh
@@ -0,0 +1,28 @@
+#!bin/bash
+
+JAVA_VERSION=17.0.4-amzn
+GRADLE_VERSION=7.3
+PKG=("zip", "unzip")
+
+for PKG in "${PKG[@]}" ; do
+  # https://stackoverflow.com/questions/592620/how-can-i-check-if-a-program-exists-from-a-bash-script
+  if ! command -v zip &> /dev/null; then
+    echo "zip is not installed. Installing..."
+    if [ -x "$(command -v apt)" ]; then
+      sudo apt update
+      sudo apt install -y zip
+    elif [ -x "$(command -v yum)" ]; then
+      sudo yum install -y zip
+    else
+      echo "Unsupported package manager. Please install 'zip' manually."
+      exit 1
+    fi
+  else,
+    echo "zip is already installed."
+  fi
+done
+
+curl -s "https://get.sdkman.io" | bash \
+  && source "${HOME}/.sdkman/bin/sdkman-init.sh" \
+  && sdk install java "${JAVA_VERSION}" \
+  && sdk install gradle ${GRADLE_VERSION}
